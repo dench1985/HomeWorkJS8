93842d2c352a85186be52e7366aac3e3a30aa45f
Den <8770532@gmail.com>
2023-07-01 14:47:06 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+let names = [
+    { id: 1, name: 'Steven', image: 'https://i.ibb.co/tXqmKKR/1.png', salary: '5000' },
+    { id: 2, name: 'Neena', image: 'https://i.ibb.co/yYxK3cq/2.png', salary: '10000' },
+    { id: 3, name: 'John', image: 'https://i.ibb.co/SNqFDbB/3.png', salary: '4500' },
+    { id: 4, name: 'Rogers', image: 'https://i.ibb.co/WnG0fVZ/4.png', salary: '3400' },
+    { id: 5, name: 'Cevin', image: 'https://i.ibb.co/vdQK54J/6.png', salary: '500' },
+    { id: 6, name: 'Alshey', image: 'https://i.ibb.co/wSJN6db/5.png', salary: '1210' },
+    { id: 7, name: 'Milki', image: 'https://i.ibb.co/JpnDnH5/7.png', salary: '1210' },
+    { id: 8, name: 'Polar', image: 'https://i.ibb.co/SnNLhgg/9.png', salary: '1210' },
+    { id: 9, name: 'Grindell', image: 'https://i.ibb.co/fGH3RPW/8.png', salary: '1210' },
+]
+
+let div_root = document.querySelector('#root')
+
+
+// for( let elem of names){
+//     const div_card = document.createElement("div");
+// const div_image = document.createElement('img')
+// const div_info = document.createElement("div");
+
+// const div_id = document.createElement("div");
+// const div_name = document.createElement("div");
+// const div_salary = document.createElement("div");
+
+
+
+// div_id.innerText = elem.id
+// div_name.innerText = elem.name
+// div_salary.innerText = elem.salary
+
+// div_card.className= 'card'
+// div_image.src = elem.image;
+// div_image.width = '350';
+// div_image.height = '350'
+
+// div_info.append(div_id,div_name,div_salary)
+// div_card.append(div_image,div_info)
+// div_root.append(div_card)
+
+// }
+
+//----------------------------------------------------------------------
+// C функцией
+
+// for (let elem of names) {
+
+//     div_root.append(crete_card(elem))
+
+// }
+
+// function crete_card(elem) {
+
+//     const div_card = document.createElement("div");
+//     const div_image = document.createElement('img')
+//     const div_info = document.createElement("div");
+
+//     const div_id = document.createElement("div");
+//     const div_name = document.createElement("div");
+//     const div_salary = document.createElement("div");
+
+
+
+//     div_id.innerText = elem.id
+//     div_name.innerText = elem.name
+//     div_salary.innerText = elem.salary
+
+//     div_card.className = 'card'
+//     div_image.src = elem.image;
+//     div_image.width = '350';
+//     div_image.height = '350'
+
+//     div_info.append(div_id, div_name, div_salary)
+//     div_card.append(div_image, div_info)
+
+//     return div_card
+// }
+
+
